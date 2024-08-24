901c00b5be28086ac33e5775008772c303425003
Ryan Pelavin <rpelavin@outlook.com>
2024-08-24 16:49:18 -0400

    version: 0.0.3 => 0.0.4; using widgetbook as example project

--- example/lib/card_payment.dart
+++ example/lib/card_payment.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:square_web_payments/square_web_payments.dart';
-import 'package:square_web_payments_widgetbook/views/payment_view.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'views/payment_view.dart';
 
 @widgetbook.UseCase(name: 'Default', type: CardPayment)
 Widget build(BuildContext context) => CardPayment(
