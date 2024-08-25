c230c28ad697628597e186a255bad0cd88d4fd63
Ryan Pelavin <rpelavin@outlook.com>
2024-08-25 13:13:29 -0400

    removing view widgets; displaying native objects as json

---
+++ example/lib/payment_view.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import 'package:square_web_payments/square_web_payments.dart';
+
+class PaymentView extends StatefulWidget {
+  final Widget? view;
+  final Future<TokenResult> Function() tokenize;
+
+  const PaymentView({super.key, required this.view, required this.tokenize});
+
+  @override
+  State<StatefulWidget> createState() => _PaymentViewState();
+}
+
+class _PaymentViewState extends State<PaymentView> {
+  TokenResult? tokenResult;
+  Error? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+        color: Colors.white,
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: widget.view == null
+                ? const Center(child: CircularProgressIndicator())
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                        Container(
+                            constraints: const BoxConstraints(minHeight: 90),
+                            child: widget.view!),
+                        const SizedBox(height: 6),
+                        TextButton(
+                            onPressed: _tokenize,
+                            child: const Text('TOKENIZE')),
+                        ...(tokenResult == null
+                            ? []
+                            : [
+                                const SizedBox(height: 6),
+                                Text(tokenResultToJson(tokenResult!))
+                              ]),
+                        ...(error == null
+                            ? []
+                            : [
+                                const SizedBox(height: 6),
+                                Text(errorToJson(error!))
+                              ])
+                      ])));
+  }
+
+  void _tokenize() {
+    setState(() {
+      tokenResult = null;
+      error = null;
+    });
+    widget
+        .tokenize()
+        .then((tokenResult) => setState(() => this.tokenResult = tokenResult))
+        .catchError((error) => setState(() => this.error = error as Error));
+  }
+
+  String errorToJson(Error error) => const JsonEncoder.withIndent('  ').convert(
+      {'name': error.name, 'message': error.message, 'stack': error.stack});
+
+  String tokenResultToJson(TokenResult tokenResult) =>
+      const JsonEncoder.withIndent('  ').convert({
+        'details': tokenResult.details == null
+            ? null
+            : {
+                'card': tokenResult.details?.card == null ? null : {},
+                'giftCard': tokenResult.details?.giftCard == null ? null : {}
+              },
+        'errors': tokenResult.errors?.toDart
+            .map((error) => {
+                  'field': error.field,
+                  'message': error.message,
+                  'type': error.type
+                })
+            .toList(),
+        'status': tokenResult.status,
+        'token': tokenResult.token
+      });
+}
