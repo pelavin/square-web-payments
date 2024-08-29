6e635d981b8cc6b01fb2939e04d0e06774fbb09f
Ryan Pelavin <ryan@pelavin.dev>
2024-08-29 07:10:31 -0400

    initial implementation from https://github.com/rpelavin/square-web-payments-app/tree/77b642c986edc5fde919ced9b47e92a4a95bc509

--- test/square_web_payments_test.dart
+++ test/square_web_payments_test.dart
@@ -1,12 +1,23 @@
+import 'dart:js_interop';
+
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:square_web_payments/square_web_payments.dart';
 
+@JSExport()
+class FakePaymentMethod {
+  String get id => 'fake_id';
+}
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  final PaymentMethod paymentMethod =
+      createJSInteropWrapper<FakePaymentMethod>(FakePaymentMethod())
+          as PaymentMethod;
+
+  testWidgets('finds an HtmlElementView widget', (tester) async {
+    await tester.pumpWidget(PaymentMethodView(paymentMethod: paymentMethod));
+    expect(find.byWidgetPredicate((widget) => widget is HtmlElementView),
+        findsOneWidget);
   });
 }
